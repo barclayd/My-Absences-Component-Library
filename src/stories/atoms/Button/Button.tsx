9a0687cdfe06e1ad58fbbcbc24a2152d5f33c877
Daniel Barclay <barclaysd@me.com>
2020-12-01 14:16:16 +0000

    Fix Storybook warnings

--- src/stories/atoms/Button/Button.tsx
+++ src/stories/atoms/Button/Button.tsx
@@ -4,18 +4,6 @@ import './button.css';
 
 export interface ButtonProps {
   /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large';
-  /**
    * Button contents
    */
   label: string;
@@ -28,11 +16,6 @@ export interface ButtonProps {
 /**
  * Primary UI component for user interaction
  */
-export const Button: React.FC<ButtonProps> = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
-}) => {
+export const Button: React.FC<ButtonProps> = ({ label }) => {
   return <ButtonContainer>{label}</ButtonContainer>;
 };
